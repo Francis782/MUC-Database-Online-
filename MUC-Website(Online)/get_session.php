ff7e3e6a3e07a1b35a06e44c91d469b2e63c9950
Francis <91157695+Francis782@users.noreply.github.com>
2025-05-26 14:57:49 -0400

    Up

--- MUC-Website(Online)/get_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION["username"])) {
-    echo "Session is working! Username: " . $_SESSION["username"];
-} else {
-    echo "Session not found! Make sure cookies are enabled.";
-}
-?>
--- MUC-Website(Online)/set_session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION["username"] = "TestUser"; // Set session variable
-
-echo "Session started and username set!<br>";
-echo "<a href='get_session.php'>Go to next page</a>";
-?>
